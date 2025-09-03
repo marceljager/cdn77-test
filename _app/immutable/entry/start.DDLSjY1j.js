2640a87d19655d4b64d99ff3c2eed326a99afe34
Marcel Jäger <mjager9@gmail.com>
2025-09-03 11:29:25 +0200

    prefix all routes with repo-name

---
+++ _app/immutable/entry/start.DDLSjY1j.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/C6dq2nax.js";export{o as load_css,r as start};
--- _app/immutable/entry/start.Dlwmk2e7.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/00cZ5j2P.js";export{o as load_css,r as start};
